6d95cb36d1b00e8a22445375e316d300a9780d6a
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-03 21:22:50 +0700

    added game

---
+++ game/mygame.js
@@ -0,0 +1,117 @@
+var quiz = [{
+  "question": "Who is the guy in the background of my homepage?",
+  "choices": ["Bill Gates", "Nusret Gocke", "Your Daddy"],
+  "correct": "Nusret Gocke"
+}, {
+  "question": "What does that guy do for a living?",
+  "choices": ["Chef / Restaurant Owner", "Web Designer", "Playing DOTA"],
+  "correct": "Chef / Restaurant Owner"
+}, {
+  "question": "What is he known as?",
+  "choices": ["Chef Bae", "Steak Bae", "Salt Bae"],
+  "correct": "Salt Bae"
+}, {
+  "question": "Who is the second president of Indonesia?",
+  "choices": ["Joko Widodo", "Suharto", "Bobby"],
+  "correct": "Suharto"
+}, {
+  "question": "Do you love me?",
+  "choices": ["Yes", "No", "Maybe"],
+  "correct": "Yes"
+}];
+
+var content = $("content");
+var questionContainer = $("question");
+var choicesContainer = $("choices");
+var scoreContainer = $("score");
+var submitBtn = $("submit");
+var currentQuestion = 0;
+var score = 0;
+var askingQuestion = true;
+
+function $(id) {
+  return document.getElementById(id);
+}
+
+function askQuestion() {
+  var choices = quiz[currentQuestion].choices;
+  var choicesHtml = "";
+  for (var i = 0; i < choices.length; i++) {
+    choicesHtml += "<input type='radio' name='quiz" + currentQuestion +
+      "' id='choice" + (i + 1) +
+      "' value='" + choices[i] + "'>" +
+      " <label for='choice" + (i + 1) + "'>" + choices[i] + "</label><br>";
+  }
+
+  // load the question
+  questionContainer.textContent = "Q" + (currentQuestion + 1) + ". " +
+    quiz[currentQuestion].question;
+
+  // load the choices
+  choicesContainer.innerHTML = choicesHtml;
+
+  // setup for the first time
+  if (currentQuestion === 0) {
+    scoreContainer.textContent = "Score: 0 right answers out of " +
+      quiz.length + " possible.";
+    submitBtn.textContent = "Submit Answer";
+  }
+}
+
+function checkAnswer() {
+  // are we asking a question, or proceeding to next question?
+  if (askingQuestion) {
+    submitBtn.textContent = "Next Question";
+    askingQuestion = false;
+
+    // determine which radio button they clicked
+    var userpick,
+      correctIndex,
+      radios = document.getElementsByName("quiz" + currentQuestion);
+    for (var i = 0; i < radios.length; i++) {
+      if (radios[i].checked) { // if this radio button is checked
+        userpick = radios[i].value;
+      }
+
+      // get index of correct answer
+      if (radios[i].value == quiz[currentQuestion].correct) {
+        correctIndex = i;
+      }
+    }
+
+    // setup if they got it right, or wrong
+    var labelStyle = document.getElementsByTagName("label")[correctIndex].style;
+    labelStyle.fontWeight = "bold";
+    if (userpick == quiz[currentQuestion].correct) {
+      score++;
+      labelStyle.color = "green";
+    } else {
+      labelStyle.color = "red";
+    }
+
+    scoreContainer.textContent = "Score: " + score + " right answers out of " +
+      quiz.length + " possible.";
+  } else { // move to next question
+    // setting up so user can ask a question
+    askingQuestion = true;
+    // change button text back to "Submit Answer"
+    submitBtn.textContent = "Submit Answer";
+    // if we're not on last question, increase question number
+    if (currentQuestion < quiz.length - 1) {
+      currentQuestion++;
+      askQuestion();
+    } else {
+      showFinalResults();
+    }
+  }
+}
+
+function showFinalResults() {
+  content.innerHTML = "<h2>You've complited the quiz!</h2>" +
+    "<h2>Below are your results:</h2>" +
+    "<h2>" + score + " out of " + quiz.length + " questions, " +
+    Math.round(score / quiz.length * 100) + "%<h2>";
+}
+
+window.addEventListener("load", askQuestion, false);
+submitBtn.addEventListener("click", checkAnswer, false);
